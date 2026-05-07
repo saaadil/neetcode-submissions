6e8f328019664a867bddeff40a098b8da92474d0
S A Aadil <shameemabbas2006@gmail.com>
2026-05-07 20:47:45 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=0;
+        int m=1;
+        int max=0;
+        while(m<prices.size()){
+            if (prices[n]<prices[m]){
+                int sum=prices[m]-prices[n];
+                if (sum>max){
+                    max=sum;
+                }
+            }
+            else if (prices[n]>prices[m]){
+                n=m;
+            }
+            m++;
+        }
+        return max;
+    }
+};
